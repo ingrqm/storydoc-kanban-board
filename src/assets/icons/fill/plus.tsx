b7427173dc7e8d8103f60e0fafc41968e19809c9
Ingram Kalina <51913097+ingrqm@users.noreply.github.com>
2023-10-16 10:51:46 +0200

    feat: icons (#5)

---
+++ src/assets/icons/fill/plus.tsx
@@ -0,0 +1,12 @@
+import type { SVGProps } from 'components/icon/icon.type';
+
+export const plus = ({ size }: SVGProps) => (
+  <svg width={size} height={size} viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g>
+      <path
+        d="M7.83331 7.33325V3.33325H9.16665V7.33325H13.1666V8.66659H9.16665V12.6666H7.83331V8.66659H3.83331V7.33325H7.83331Z"
+        fill="currentColor"
+      />
+    </g>
+  </svg>
+);
