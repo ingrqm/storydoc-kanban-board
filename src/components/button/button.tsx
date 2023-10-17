c06f9adc21e14c318d28f0fc9d9365109ffaa6b2
ingrqm <contact@imaxart.com>
2023-10-17 23:22:36 +0200

    feat: forward ref into button

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,27 +1,32 @@
-import type { ReactNode } from 'react';
+import { forwardRef, type ReactNode } from 'react';
 
 import * as Styled from './button.styled';
 
 type ButtonProps = {
   children: ReactNode;
-  variant?: 'default' | 'primary' | 'ghost' | 'disabled';
+  variant?: 'default' | 'primary' | 'ghost';
   size?: 'xs' | 'sm' | 'md' | 'lg';
   onClick?: () => void;
   isBlock?: boolean;
   isIcon?: boolean;
+  disabled?: boolean;
 };
 
-export const Button = ({
-  children,
-  variant = 'default',
-  size = 'sm',
-  onClick,
-  isBlock = false,
-  isIcon = false,
-}: ButtonProps) => {
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(function Button(
+  { children, variant = 'default', size = 'sm', onClick, isBlock = false, isIcon = false, disabled = false },
+  ref,
+) {
   return (
-    <Styled.Button $variant={variant} $size={size} onClick={onClick} $isBlock={isBlock} $isIcon={isIcon}>
+    <Styled.Button
+      ref={ref}
+      $variant={variant}
+      $size={size}
+      onClick={onClick}
+      $isBlock={isBlock}
+      $isIcon={isIcon}
+      disabled={disabled}
+    >
       {children}
     </Styled.Button>
   );
-};
+});
